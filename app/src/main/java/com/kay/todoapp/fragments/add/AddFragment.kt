262b98534ab82c63b6f85a40fd4972e03f10012f
kay <kayshiro@live.no>
2021-07-15 18:33:07 +0200

    All 3 menu done

--- app/src/main/java/com/kay/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/kay/todoapp/fragments/add/AddFragment.kt
@@ -1,21 +1,36 @@
 package com.kay.todoapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.kay.todoapp.R
+import com.kay.todoapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+    // TODO: set up the view binding
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        // Set Menu
+        setHasOptionsMenu(true)
+
+
+        return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
