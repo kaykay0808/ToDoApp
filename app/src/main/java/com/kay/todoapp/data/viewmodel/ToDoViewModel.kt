a7d9d4824896e013a0a32dd81d3ce42d82aa2382
kay <kayshiro@live.no>
2021-08-05 11:57:51 +0200

    Logic to addFragment

---
+++ app/src/main/java/com/kay/todoapp/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,32 @@
+package com.kay.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.kay.todoapp.data.ToDoDao
+import com.kay.todoapp.data.ToDoDatabase
+import com.kay.todoapp.data.models.ToDoData
+import com.kay.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel (application: Application):  AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository = ToDoRepository(toDoDao)
+
+    private val gelAllData: LiveData<List<ToDoData>>
+
+    init {
+        var repository = ToDoRepository(toDoDao)
+        gelAllData = repository.getAllData
+
+    }
+
+    fun insertData(toDoData: ToDoData){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+}
--- app/src/main/java/com/kay/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/kay/todoapp/fragments/add/AddFragment.kt
@@ -1,19 +1,28 @@
 package com.kay.todoapp.fragments.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.kay.todoapp.R
+import com.kay.todoapp.data.models.Priority
+import com.kay.todoapp.data.models.ToDoData
+import com.kay.todoapp.data.viewmodel.ToDoViewModel
 import com.kay.todoapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
-    // TODO: set up the view binding
     private var _binding: FragmentAddBinding? = null
     private val binding get() = _binding!!
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +34,62 @@ class AddFragment : Fragment() {
         // Set Menu
         setHasOptionsMenu(true)
 
-
         return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_add) {
+            insertDataToDb()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDb() {
+        val mTitle = binding.titleEt.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEt.text.toString()
+
+        val validation = verifyDataFromUser(mTitle, mDescription)
+        if (validation) {
+            // if validation return true, only then we can insert data to database
+            val newData = ToDoData(
+                0,
+                mTitle,
+                parsePriority(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
+            // Navigate Back
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }else{
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun verifyDataFromUser(title: String, description: String): Boolean {
+        return if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
+            false
+        } else !(title.isEmpty() || description.isEmpty())
+    }
+
+    private fun parsePriority(priority: String): Priority {
+        return when (priority) {
+            "High Priority" -> {
+                Priority.HIGH
+            }
+            "Medium Priority" -> {
+                Priority.MEDIUM
+            }
+            "Low Priority" -> {
+                Priority.LOW
+            }
+            else -> Priority.LOW
+        }
     }
 
     override fun onDestroyView() {
